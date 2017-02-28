f1ba61de309d7e0ef6fcaeb2f671e4ce86305c63
Aaron M <aaron.m.manley@gmail.com>
2017-02-28 10:16:54 -0800

    more configingrationggssss

---
+++ Unit-01/03-webpack/webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // The absolute path to your project
+  context: __dirname + "/",
+  // the entry point for our app
+  entry: './main.js',
+  // where to put the compiled output (what our script tag will link to)
+  output: {
+    // where does it go?
+    path: __dirname + "/",
+    // what is the file called?
+    filename: 'bundle.js'
+  },
+  // how can we debug our bundle? for production, we can use 'source-map'
+  devtool: 'inline-source-map',
+  module: {
+    rules: [{
+      //Check for all js files
+      test: /\.js$/,
+      // Don't include node_modules directory in the search for js files
+      exclude: /node_modules/,
+      // Use the babel-loader plugin to transpile the javascript
+      use: [{
+        loader: 'babel-loader',
+        options: { presets: ['es2015'] }
+      }]
+    }]
+  }
+};
