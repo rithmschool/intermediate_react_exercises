a28a552f453c5279ba0faccf700ea3017ec4d466
Katie Krieger <katielkrieger@gmail.com>
2017-06-13 23:16:39 -0700

    In the middle of updating cart quantity logic

--- Unit-02/01-react-router/makeup-app/src/Cart.js
+++ Unit-02/01-react-router/makeup-app/src/Cart.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Cart.css';
 
 class Cart extends Component {
 
@@ -13,12 +14,21 @@ class Cart extends Component {
 
   render() {
     console.log("items",this.props.items);
+
     // NEED TO PUT IN AN ARRAY OF OBJECTS TO SHOW COUNTS
     // AND ONLY SHOW EACH ITEM ONCE (WITH QUANTITY)
+    let items = this.props.items;
+    let counts = {};
+    for (let i = 0; i < items.length; i++) {
+        counts[items[i]] = 1 + (counts[items[i]] || 0);
+    }
+    console.log("counts",counts);
 
-    let tableRows = this.props.items.map((item, i) => (
+    
+
+    let tableRows = items.map((item, i) => (
       <tr key={i}>
-        <th scope="row">1</th>
+        <th scope="row">{counts[items[i]]}</th>
         <td>{item.props.name}</td>
         <td>{item.props.price}</td>
         <td>
@@ -38,6 +48,7 @@ class Cart extends Component {
     this.props.items.forEach((item, i) => {
       total += +item.props.price;
     })
+    total = total.toFixed(2);
 
     return (
       <div>
